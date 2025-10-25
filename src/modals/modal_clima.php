0b6849a137593210057bd47016af428359b69918
Vinícius Pereira de Morais <132583732+vinipera@users.noreply.github.com>
2025-10-25 13:09:05 -0300

    Create modal_clima.php

---
+++ src/modals/modal_clima.php
@@ -0,0 +1,9 @@
+<!-- Modal para Exibir Clima -->
+<div id="modalClima" class="modal">
+    <div class="modal-content clima-modal-content">
+        <span class="close" onclick="closeModal('modalClima')">&times;</span>
+        <div class="clima-body" id="clima-body">
+            <!-- Conteúdo do clima será carregado aqui -->
+        </div>
+    </div>
+</div>
